52d35321a113360d27cfd184a147fc1403c4b201
atharavmishra <atharvmishra77@gmail.com>
2024-07-27 19:01:30 +0530

    initial commit

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -6,17 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -24,9 +24,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +38,7 @@ import com.example.jetpackcompose.repository.ContactDatabase
 import com.example.jetpackcompose.repository.ContactEntity
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     private lateinit var database: ContactDatabase
@@ -52,8 +56,6 @@ class MainActivity : ComponentActivity() {
             database.contactDao().insertContact(contactEntity)
         }
 
-//        var contactName: String? = null
-
         database.contactDao().getContact().observe(this) {
             Toast.makeText(this@MainActivity, it[0].name, Toast.LENGTH_SHORT).show()
 
@@ -65,19 +67,28 @@ class MainActivity : ComponentActivity() {
             val painter = painterResource(id = R.drawable.hanuman)
             Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(5.dp)
-                    .height(200.dp),
             ) {
-                Text(
-                    text = "Hello",
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily
-                )
+                ButtonBox(Modifier.fillMaxSize())
             }
 
         }
     }
+}
+
+@Composable
+fun ButtonBox(modifier: Modifier = Modifier) {
+    val color = remember {
+        mutableStateOf(Color.Cyan)
+    }
+    Box(modifier = modifier
+        .background(color.value)
+        .clickable {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat()
+            )
+        })
 }
 
 @Composable
@@ -135,8 +146,28 @@ fun ImageCard(
 @Composable
 fun GreetingPreview() {
     val painter = painterResource(id = R.drawable.hanuman)
-    ImageCard(
-        painter = painter, contentDescription = "Lord Hanuman", title = "Lord Hanuman"
+    Text(
+        text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Green,
+                    fontSize = 50.sp,
+                )
+            ) {
+                append("H")
+            }
+            append("ello")
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Green,
+                    fontSize = 50.sp,
+                )
+            ) {
+                append("W")
+            }
+            append("orld")
+        },
+        fontSize = 30.sp,
     )
 
 
